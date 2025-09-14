6d22c6afed0e799350a26b9a89cd5368dd1c6a29
Carlos479067 <carlos.martinez11@my.utsa.edu>
2025-09-14 02:22:37 -0500

    Removed logout from header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,27 +1,12 @@
 import Logo from "../assets/eagle.png";
-import {useNavigate} from "react-router-dom";
-import logoutImg from "../assets/logout.png";
 
-export default function Header({loggedInUser, setLoggedInUser}) {
-
-    const navigate = useNavigate();
-
-    function logout() {
-        // Reset user and navigate back to login page
-        localStorage.removeItem("jwtToken");
-        setLoggedInUser(null);
-        navigate("/login");
-    }
+export default function Header() {
 
     return (
         <header id={"header"}>
             <div id={"headerContent"}>
                 <img id={"headerImage"} src={Logo} alt={"logo"} />
-                <h3>Welcome {loggedInUser?.firstName}</h3>
-                <div id={"logoutContainer"}>
-                    <img id={"headerLogout"} src={logoutImg} alt={"logout"} onClick={logout}/>
-                    <h4 onClick={logout}>Logout</h4>
-                </div>
+                <h3>Welcome</h3>
             </div>
         </header>
     )
