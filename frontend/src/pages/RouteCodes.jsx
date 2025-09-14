64b2fae3d6c7c3719faf6716e1dd9ae7a6bea32f
Carlos479067 <carlos.martinez11@my.utsa.edu>
2025-09-14 02:16:25 -0500

    Added routeCodes.jsx to display a routes information

---
+++ frontend/src/pages/RouteCodes.jsx
@@ -0,0 +1,63 @@
+import {useState} from "react";
+import {useParams} from "react-router-dom";
+import {useEffect} from "react";
+
+export default function RouteCodes() {
+
+    const [addresses, setAddresses] = useState([]);
+    const {routeNumber} = useParams();
+
+    function handleAddresses() {
+        const getUrl = `${import.meta.env.VITE_API_URL}/api/codes/${routeNumber}`;
+
+        const addressObj = {
+            method: "GET"
+        }
+
+        fetch(getUrl, addressObj)
+            .then((response) => {
+                if(!response.ok) {
+                    throw new Error(`Network response error: ${response.status}`);
+                }
+                return response.json();
+            })
+            .then((data) => {
+                setAddresses(data);
+            })
+            .catch((error) => {
+                throw new Error(`There was a problem with the fetch request: ${error.status}`);
+            });
+    }
+
+    function RenderAddress({addressObj}) {
+        return (
+            <li>
+                <div className={"container"}>
+                    {addressObj.streetNumber ? <h2>{addressObj.streetNumber ? addressObj.streetNumber : ""} {addressObj.streetNumber ? addressObj.streetName : ""} {addressObj.streetNumber ? addressObj.cityName : ""} {addressObj.streetNumber ? addressObj.zipCode : ""}</h2> : ""}
+                    {addressObj.neighborhood ? <h2>{addressObj.neighborhood ? "Neighborhood: " : ""}{addressObj.neighborhood}</h2> : ""}
+                    {addressObj.gateCode ? <h3>{addressObj.gateCode ? "Gate Code: " : ""}{addressObj.gateCode}</h3> : ""}
+                    {addressObj.mailRoomCode ? <h3>{addressObj.mailRoomCode ? "Mailroom Code: " : ""}{addressObj.mailRoomCode}</h3> : ""}
+                    {addressObj.locker_code ? <h3>{addressObj.locker_code ? "Locker Room Code: " : ""}{addressObj.locker_code}</h3> : ""}
+                </div>
+            </li>
+        )
+    }
+
+
+    useEffect(() => {
+        handleAddresses()
+    }, []);
+
+    return (
+        <main>
+            <div>
+                <h2 id={"mainTitle"}>Route {routeNumber} Codes</h2>
+                <ul>
+                    {addresses.map((address) => {
+                        return <RenderAddress addressObj={address} key={address.address_id}/>
+                    })}
+                </ul>
+            </div>
+        </main>
+    )
+}
