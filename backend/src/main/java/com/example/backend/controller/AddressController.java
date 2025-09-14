9ce8a4adf408c01ef852d4956c8fdfceee62afb4
Carlos479067 <carlos.martinez11@my.utsa.edu>
2025-09-14 02:36:12 -0500

    Removed /api from controller for testing

--- backend/src/main/java/com/example/backend/controller/AddressController.java
+++ backend/src/main/java/com/example/backend/controller/AddressController.java
@@ -1,11 +1,9 @@
 package com.example.backend.controller;
-
 import com.example.backend.dto.AddressDto;
 import com.example.backend.service.AddressService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
 
 @RestController
