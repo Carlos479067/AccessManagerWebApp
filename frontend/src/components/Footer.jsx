bd7dd78e11f880d03dd2bed2421e8dd0eb49c038
Carlos479067 <carlos.martinez11@my.utsa.edu>
2025-09-14 02:12:18 -0500

    Added images for logo and other various images. Created Footer

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+export default function Footer () {
+
+    return (
+        <footer>
+            <div id={"topFooter"}></div>
+            <div id={"lowFooter"}>
+            </div>
+        </footer>
+    )
+}
