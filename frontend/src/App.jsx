54fc3864cb8fbbea58d854ca61ee8922f6aa9088
Carlos479067 <carlos.martinez11@my.utsa.edu>
2025-09-14 02:19:46 -0500

    Edited App.jsx to include useState to use as a prop to different classes. Added Route paths.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,40 @@
-
 import './App.css'
+import Header from "./components/Header.jsx";
+import Navbar from "./components/Navbar.jsx";
+import Footer from "./components/Footer.jsx";
 import Home from "./pages/Home.jsx";
+import RouteCodes from "./pages/RouteCodes.jsx";
+import Forms from "./pages/Forms.jsx";
+import RouteSplits from "./pages/RouteSplits.jsx";
+import {useState} from "react";
+import {Route, Routes} from "react-router-dom";
 
 function App() {
 
-  return (
-    <>
-        <Home />
-    </>
-  )
+    const [searchResults, setSearchResults] = useState("");
+    function updateState(data) {
+        setSearchResults(data);
+    }
+
+    function resetState() {
+        setSearchResults([]);
+    }
+
+    return (
+        <div className={"pageContainer"}>
+            <Header searchResults={searchResults}/>
+            <Navbar updateState={updateState} resetState={resetState}/>
+            <main className={"appMain"}>
+                <Routes>
+                    <Route path={"/"} element={<Home searchResults={searchResults}/>}/>
+                    <Route path={"/forms"} element={<Forms />}/>
+                    <Route path={"/codes/:routeNumber"} element={<RouteCodes/>}/>
+                    <Route path={"/splits/:routeNumber"} element={<RouteSplits/>}/>
+                </Routes>
+            </main>
+            <Footer/>
+        </div>
+    )
 }
 
 export default App
