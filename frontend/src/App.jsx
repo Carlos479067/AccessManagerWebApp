e61d39929c11ae52b0c066c3f65f2ca61193de3b
Carlos479067 <carlos.martinez11@my.utsa.edu>
2025-09-17 22:24:06 -0500

    Updated user persistence to prevent refresh from logging out user. Updated mobile header layout.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,11 +7,18 @@ import RouteCodes from "./pages/RouteCodes.jsx";
 import Forms from "./pages/Forms.jsx";
 import RouteSplits from "./pages/RouteSplits.jsx";
 import {useState} from "react";
-import {Route, Routes} from "react-router-dom";
+import {Navigate, Route, Routes} from "react-router-dom";
+import Login from "./pages/Login.jsx";
 
 function App() {
 
     const [searchResults, setSearchResults] = useState("");
+    const [loggedInUser, setLoggedInUser] = useState(() => {
+        // Looks in the browser’s storage.
+        const savedUser = localStorage.getItem("loggedInUser");
+        return savedUser ? JSON.parse(savedUser) : null;
+    })
+
     function updateState(data) {
         setSearchResults(data);
     }
@@ -20,15 +27,21 @@ function App() {
         setSearchResults([]);
     }
 
+    function updateLoggedInUser(data) {
+        setLoggedInUser(data);
+    }
+
     return (
         <div className={"pageContainer"}>
-            <Header searchResults={searchResults}/>
-            <Navbar updateState={updateState} resetState={resetState}/>
+            <Header searchResults={searchResults} setLoggedInUser={setLoggedInUser} loggedInUser={loggedInUser}/>
+            <Navbar updateState={updateState} resetState={resetState} loggedInUser={loggedInUser}/>
             <main className={"appMain"}>
                 <Routes>
-                    <Route path={"/"} element={<Home searchResults={searchResults}/>}/>
+                    <Route path={"/"} element={loggedInUser ? <Navigate to={"/home"} replace={true} /> : <Login setLoggedInUser={updateLoggedInUser} />}/>
+                    <Route path={"/home"} element={loggedInUser ? <Home searchResults={searchResults} loggedInUser={loggedInUser} /> : <Login setLoggedInUser={updateLoggedInUser} />}
+                    />
                     <Route path={"/forms"} element={<Forms />}/>
-                    <Route path={"/codes/:routeNumber"} element={<RouteCodes/>}/>
+                    <Route path={"/codes/:routeNumber"} element={<RouteCodes searchResults={searchResults}/>}/>
                     <Route path={"/splits/:routeNumber"} element={<RouteSplits/>}/>
                 </Routes>
             </main>
--- frontend/src/pages/RouteCodes.jsx
+++ frontend/src/pages/RouteCodes.jsx
@@ -2,13 +2,34 @@ import {useState} from "react";
 import {useParams} from "react-router-dom";
 import {useEffect} from "react";
 
-export default function RouteCodes() {
+export default function RouteCodes({searchResults}) {
 
     const [addresses, setAddresses] = useState([]);
     const {routeNumber} = useParams();
 
+    let contentToRender = <></>;
+
+    if(searchResults.length === 0) {
+        contentToRender =
+            <div>
+                <h2 id={"mainTitle"}>Route {routeNumber} Codes</h2>
+                <ul>
+                    {addresses.map((mainAddress) => {
+                        return <RenderAddress addressMainObj={mainAddress} key={mainAddress.address_id}/>
+                    })}
+                </ul>
+            </div>
+    } else {
+        contentToRender =
+            <>
+                <div className={"main"}>
+                    <MapAddress searchAddress={searchResults}/>
+                </div>
+            </>
+    }
+
     function handleAddresses() {
-        const getUrl = `${import.meta.env.VITE_API_URL}/api/codes/${routeNumber}`;
+        const getUrl = `http://localhost:8080/api/codes/${routeNumber}`;
 
         const addressObj = {
             method: "GET"
@@ -16,7 +37,7 @@ export default function RouteCodes() {
 
         fetch(getUrl, addressObj)
             .then((response) => {
-                if(!response.ok) {
+                if (!response.ok) {
                     throw new Error(`Network response error: ${response.status}`);
                 }
                 return response.json();
@@ -29,15 +50,50 @@ export default function RouteCodes() {
             });
     }
 
-    function RenderAddress({addressObj}) {
+    function MapAddress({searchAddress}) {
+
+        return (
+            <>
+                {searchAddress.length > 0 ? (
+                    <div>
+                        <ul>
+                            {searchAddress.map((searchAddresses) => (
+                                <RenderSearchAddress addressSearchObj={searchAddresses} key={searchAddresses.address_id}/>
+                            ))}
+                        </ul>
+                    </div>
+
+                ) : (<p>No address found</p>)
+                }
+            </>
+
+        )
+    }
+
+    function RenderAddress({addressMainObj}) {
         return (
             <li>
                 <div className={"container"}>
-                    {addressObj.streetNumber ? <h2>{addressObj.streetNumber ? addressObj.streetNumber : ""} {addressObj.streetNumber ? addressObj.streetName : ""} {addressObj.streetNumber ? addressObj.cityName : ""} {addressObj.streetNumber ? addressObj.zipCode : ""}</h2> : ""}
-                    {addressObj.neighborhood ? <h2>{addressObj.neighborhood ? "Neighborhood: " : ""}{addressObj.neighborhood}</h2> : ""}
-                    {addressObj.gateCode ? <h3>{addressObj.gateCode ? "Gate Code: " : ""}{addressObj.gateCode}</h3> : ""}
-                    {addressObj.mailRoomCode ? <h3>{addressObj.mailRoomCode ? "Mailroom Code: " : ""}{addressObj.mailRoomCode}</h3> : ""}
-                    {addressObj.locker_code ? <h3>{addressObj.locker_code ? "Locker Room Code: " : ""}{addressObj.locker_code}</h3> : ""}
+                    {addressMainObj.streetNumber ? <h2>{addressMainObj.streetNumber ? addressMainObj.streetNumber : ""} {addressMainObj.streetNumber ? addressMainObj.streetName : ""} {addressMainObj.streetNumber ? addressMainObj.cityName : ""} {addressMainObj.streetNumber ? addressMainObj.zipCode : ""}</h2> : ""}
+                    {addressMainObj.neighborhood ? <h2>{addressMainObj.neighborhood ? "Neighborhood: " : ""}{addressMainObj.neighborhood}</h2> : ""}
+                    {addressMainObj.gateCode ? <h3>{addressMainObj.gateCode ? "Gate Code: " : ""}{addressMainObj.gateCode}</h3> : ""}
+                    {addressMainObj.mailRoomCode ? <h3>{addressMainObj.mailRoomCode ? "Mailroom Code: " : ""}{addressMainObj.mailRoomCode}</h3> : ""}
+                    {addressMainObj.locker_code ? <h3>{addressMainObj.locker_code ? "Locker Room Code: " : ""}{addressMainObj.locker_code}</h3> : ""}
+                </div>
+            </li>
+        )
+    }
+
+    function RenderSearchAddress({addressSearchObj}) {
+        return (
+            <li>
+                <div className={"searchContainer"}>
+                    {addressSearchObj.streetNumber ? <h2>{addressSearchObj.streetNumber ? addressSearchObj.streetNumber : ""} {addressSearchObj.streetNumber ? addressSearchObj.streetName : ""} {addressSearchObj.streetNumber ? addressSearchObj.cityName : ""} {addressSearchObj.streetNumber ? addressSearchObj.zipCode : ""}</h2> : ""}
+                    <h2>Route: {addressSearchObj.routeNumber}</h2>
+                    {addressSearchObj.neighborhood ? <h3>{addressSearchObj.neighborhood ? "Neighborhood: " : ""}{addressSearchObj.neighborhood}</h3> : ""}
+                    {addressSearchObj.gateCode ? <h3>{addressSearchObj.gateCode ? "Gate Code: " : ""}{addressSearchObj.gateCode}</h3> : ""}
+                    {addressSearchObj.mailRoomCode ? <h3>{addressSearchObj.mailRoomCode ? "Mailroom Code: " : ""}{addressSearchObj.mailRoomCode}</h3> : ""}
+                    {addressSearchObj.locker_code ? <h3>{addressSearchObj.locker_code ? "Locker Room Code: " : ""}{addressSearchObj.locker_code}</h3> : ""}
                 </div>
             </li>
         )
@@ -50,14 +106,7 @@ export default function RouteCodes() {
 
     return (
         <main>
-            <div>
-                <h2 id={"mainTitle"}>Route {routeNumber} Codes</h2>
-                <ul>
-                    {addresses.map((address) => {
-                        return <RenderAddress addressObj={address} key={address.address_id}/>
-                    })}
-                </ul>
-            </div>
+            {contentToRender}
         </main>
     )
 }
