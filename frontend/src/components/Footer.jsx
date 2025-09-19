a80ddebce331e8c95808b64c3a119cf8ec93ed54
Carlos479067 <carlos.martinez11@my.utsa.edu>
2025-09-18 23:07:43 -0500

    Edited Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,7 +3,7 @@ export default function Footer () {
     return (
         <footer>
             <div id={"topFooter"}>
-                <h3 style={{color: "black", alignItems: "center"}}>This Weather Sucks</h3>
+                <h3 style={{display: "flex",  color: "black", alignItems: "center"}}>This Weather Sucks</h3>
             </div>
             <div id={"lowFooter"}>
                 <p>Copyright: Monster Route 34</p>
