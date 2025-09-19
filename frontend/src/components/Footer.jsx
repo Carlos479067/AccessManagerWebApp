a13070555e739ec4dbcfc797349a64557d3cb998
Carlos479067 <carlos.martinez11@my.utsa.edu>
2025-09-18 23:05:01 -0500

    Edited Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer () {
 
     return (
         <footer>
-            <div id={"topFooter"}><h2 style={{color: "black"}}>This Weather Sucks</h2></div>
+            <div id={"topFooter"}><h3 style={{color: "black", alignItems: "center"}}>This Weather Sucks</h3></div>
             <div id={"lowFooter"}>
             </div>
         </footer>
