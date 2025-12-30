940b4389edc645d65bc1f42522ea230a6ff02881
Carlos479067 <carlos.martinez11@my.utsa.edu>
2025-12-29 22:33:39 -0600

    Edit Text in Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,10 +5,10 @@ export default function Footer () {
     return (
         <footer>
             <div id={"topFooter"}></div>
-            <div id={"lowFooter"}>
+            <div id={"lowFooter"} style={{}}>
                 <div style={{display: "flex", justifyContent: "center"}}>
-                    <img style={{width: "20px"}} src={copyRightLogo} alt={"copy right logo"}/>
-                    <p style={{color: "white", alignItems: "center", paddingLeft: "5px"}}>C.H.M</p>
+                    <img style={{width: "20px", paddingTop: "1px"}} src={copyRightLogo} alt={"copy right logo"}/>
+                    <p style={{color: "white", paddingLeft: "6px", fontSize: "15px"}}>C.H.M</p>
                 </div>
             </div>
         </footer>
