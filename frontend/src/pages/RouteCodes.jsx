0af830a8acdfa36af235bc1cc6ea1dde2058f94d
Carlos479067 <carlos.martinez11@my.utsa.edu>
2026-02-10 02:34:09 -0600

    Update button, labels, input in RouteCodes.jsx

--- frontend/src/pages/RouteCodes.jsx
+++ frontend/src/pages/RouteCodes.jsx
@@ -255,6 +255,7 @@ export default function RouteCodes({searchResults}) {
                             </li>
                             <li><button className={"removeSubmitBtn"} type={"submit"}>Submit</button></li>
                             <li>{address.length > 0 && <h2 style={{color: "red"}}>Are you sure you want to delete this address?</h2>}</li>
+                            <li>{address.length > 0 && <SearchAddressToRemove searchResult={address} />}</li>
                         </ul>
                     </form>
                 )}
